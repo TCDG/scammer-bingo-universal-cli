4225eeb13340e2d3fa024a3a21372d53ca10f081
Dominik Madarasz <madaraszd@mail.ru>
2016-08-25 07:48:08 +0200

    Working build.

--- bingo.cpp
+++ bingo.cpp
@@ -1,7 +1,7 @@
 #define DZM_ELEVATED
 #define DZM_DEVEL
 
-#include "src/dzm.hpp"
+#include "dzm/src/dzm.hpp"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +13,34 @@
  */
 
 int
-main(int argc, char** argv)
+main()
 {
-    test_init(argc, argv);
     
+    GlobalArena = (MEMORY_ARENA *)malloc(sizeof(MEMORY_ARENA));
+    
+    if(!GlobalArena)
+    {
+         printf("Memory initialization error!\n");
+         exit(-1);
+    }
+    
+    initialize_arena(GlobalArena, MAX_VM_SIZE, malloc(MAX_VM_SIZE));
+    
+    init_mem();
+    
+    init_logging();
+    FILE *Log = fopen("dzm_log.txt", "w");
+    set_log_output(Log);
+    set_log_verbose(1);
+    
+    init_defs();
+    test_load_file("std/stdlib.scm");
+    
+    test_load_file("bingo.scm");
+
+    // TODO(zaklaus): Get rid of it as soon as we implement the cli.
+    test_repl();
+    
+    fclose(Log);
     return(0);
 }
